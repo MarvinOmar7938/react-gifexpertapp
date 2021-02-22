0b1ed4bc5776d5c8a2cfae8e4b833e22be81f929
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2021-02-22 19:46:31 -0400

    fetch API

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,19 +1,11 @@
 import { useState } from "react";
 import React from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    // const categories = ['One Punch','Samurai X','Dragon Ball'];
-
-    const [categories, setcategories] = useState(['One Punch','Samurai X','Dragon Ball']);
-
-    // const handleAdd = () => {
-    //     // para que sea el primero
-    //     // setcategories(['HunterXHunter',...categories]);
-
-    //     setcategories(cats=>[...cats,'HunterXHunter']);
-    // }
+    const [categories, setcategories] = useState(['Dragon Ball']);
 
   return (
     <>
@@ -23,9 +15,12 @@ export const GifExpertApp = () => {
 
       <ol>
           {
-              categories.map( category =>{
-                  return <li key={ category }>{ category }</li>
-              })
+              categories.map( category =>(
+                <GifGrid
+                  key = { category }
+                  category = { category }
+                />
+              ))
           }
       </ol>
     </>
