5cb2d7ee51fbd82fc756916dcbf49b9def184085
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2021-02-22 16:30:45 -0400

    list of categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import React from "react";
 
 export const GifExpertApp = () => {
+
+    // const categories = ['One Punch','Samurai X','Dragon Ball'];
+
+    const [categories, setcategories] = useState(['One Punch','Samurai X','Dragon Ball']);
+
+    const handleAdd = () => {
+        // para que sea el primero
+        // setcategories(['HunterXHunter',...categories]);
+
+        setcategories(cats=>[...cats,'HunterXHunter']);
+    }
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+      
+        <button onClick={ handleAdd }>Agregar</button>
+
+      <ol>
+          {
+              categories.map( category =>{
+                  return <li key={ category }>{ category }</li>
+              })
+          }
+      </ol>
     </>
   );
 };
