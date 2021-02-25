0c3f4f8b880b632dc1b77c46f47abee6e2d2c840
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2021-02-25 16:53:25 -0400

    images

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,35 +1,43 @@
-import React, { useState ,useEffect} from 'react'
+import React, { useEffect, useState } from "react";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
 
-    const [count, setCount] = useState(0);
+  useEffect(() => {
+    getGifs();
+  }, []);
 
-    useEffect(() => {
-        getGifs();
-    },[])
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=goku&limit=10&api_key=KkUUhbt3Q1aLg3AItKMwvsKiGOxZUov6";
+    const resp = await fetch(url);
+    const { data } = await resp.json();
 
-    const getGifs = async() =>{
-        const url = 'https://api.giphy.com/v1/gifs/search?q=goku&limit=10&api_key=KkUUhbt3Q1aLg3AItKMwvsKiGOxZUov6';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
 
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
+    console.log(gifs);
+    setImages(gifs);
+  };
 
-        console.log(gifs);
-    }
+  return (
+    <div>
+      <h3>{category}</h3>
 
-
-    return (
-        <div>
-            <h3>{ category }</h3>
-            <h3>{ count }</h3>
-            <button onClick={()=> setCount(count+1)}></button>
-        </div>
-    )
-}
+      {
+        images.map( img => (
+            <GifGridItem 
+                key={img.id}
+                { ...img}
+            />
+        ))
+      }
+    </div>
+  );
+};
