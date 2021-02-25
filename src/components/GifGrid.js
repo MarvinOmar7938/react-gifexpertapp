4632bdeac912639fe4f9da23c79762bc290ecbc5
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2021-02-25 18:39:26 -0400

    useFetchGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-  // const [images, setImages] = useState([]);
-  
-  const {data,loading} = useFetchGifs();
-  console.log(loading);
-  console.log(data);
 
-  // useEffect(() => {
-  //   getGifs(category)
-  //   .then(setImages);
-  // }, [category]);
+  const {data:images,loading} = useFetchGifs( category );
 
   return (
       <>
         <h3>{category}</h3>
-        { loading ? 'Cargando...' : 'Data cargada' }
 
-        {/* <div className="card-grid">
-            {
-                images.map( img => (
-                    <GifGridItem 
-                        key={img.id}
-                        { ...img}
-                    />
-                ))
-            }
-            </div> */}
+        { loading && <p>Loading</p> }
+
+        <div className="card-grid">
+          {
+              images.map( img => (
+                  <GifGridItem 
+                      key={img.id}
+                      { ...img}
+                  />
+              ))
+          }
+        </div>
       </>
     
   );
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-
-export const useFetchGifs = () => {
+import { useEffect, useState } from 'react'
+import { getGifs } from '../helpers/getGifs'
+export const useFetchGifs = ( category ) => {
     const [state, setstate] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout(() => {
-        setstate({
-            data: [1,2,3,4,5,6,7],
-            loading:false
+    useEffect(()=>{
+
+        getGifs( category )
+        .then( imgs => {
+            setTimeout(() => {
+                console.log(imgs);
+                setstate({
+                    data: imgs,
+                    loading: false
+                })
+            },3000);
         })
-    }, 3000);
+    },[category])
     return state;   // {data:[], loading: true};
 }
