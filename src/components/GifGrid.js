3c260e4fe7e69dc384dc23d6989197349a56509f
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2021-02-25 17:45:23 -0400

    helpers

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,30 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?q=goku&limit=10&api_key=KkUUhbt3Q1aLg3AItKMwvsKiGOxZUov6";
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-
-    console.log(gifs);
-    setImages(gifs);
-  };
+    getGifs(category)
+    .then(setImages);
+  }, [category]);
 
   return (
       <>
